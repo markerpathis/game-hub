0e6eb4de18f1ed1f862c72165eb54c3935125ebd
Parker Mathis <parkerjmathis@gmail.com>
2023-10-21 14:02:39 -0700

    Built navbar with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Nav
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
